30751f7e7b8c663049b3f98f88c5b706884e5417
Premanjita Barua <premanjitabarua@gmail.com>
2025-02-16 21:50:32 +0600

    Create PrimeNumberChecker.java

---
+++ PrimeNumberChecker.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+
+    // Method to check if a number is prime
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false; // Prime numbers are greater than 1
+        }
+        
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false; // If divisible by any number other than 1 and itself, not prime
+            }
+        }
+        
+        return true; // The number is prime
+    }
+}
